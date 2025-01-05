6e8703847122cf80b63d4ccb6965afb15196b016
Siddhant Singh <68225026+sidpreneur@users.noreply.github.com>
2025-01-05 08:13:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const myButton = document.querySelector('#myButton');
+const apiKey = '91bb49e733f6cc6cc0a83cdc87379e23';
+const result = document.getElementById("result");
+
+myButton.addEventListener('click', () => {
+  const city = document.querySelector('#city').value;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.weather) {
+        console.log("Weather Data:", data);
+        if((data.main.temp - 273.15) < 10){
+            var emoji='❄️';
+        }
+        else if((data.main.temp - 273.15) < 25){
+            var emoji='☁️'
+        }
+        else{
+            var emoji='☀️';
+        }
+        let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: data.coord.lat, lng: data.coord.lon },
+    zoom: 8,
+  });
+}
+
+
+
+        result.innerHTML = `
+          <h3>Weather in ${data.name}, ${data.sys.country}</h3>
+          <h5>Coordinates: ${data.coord.lon} , ${data.coord.lat}</h5>
+          <p>Weather: ${data.weather[0].description} ${emoji}</p>
+          <p>Temperature: ${(data.main.temp - 273.15).toFixed(2)}°C</p>
+          ${initMap()}
+        `;
+      } else {
+        console.error("Error:", data.message);
+      }
+    })
+    .catch(error => {
+      console.error("Fetch Error:", error);
+      result.innerHTML = `<p>Error: ${error.message}</p>`;
+    });
+});
